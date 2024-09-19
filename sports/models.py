6740df5ae8ec59fffa5f287a4752a5a6e0d8871d
Denys Piuro <denys.pyuro@gmail.com>
2024-09-19 12:09:54 +0300

    Created models and added initial migrations

--- sports/models.py
+++ sports/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Sport(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    age = models.IntegerField(null=True, blank=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='sports_user_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='sports_user_permissions_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
+
+class Field (models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    location = models.CharField(max_length=255)
+    sports = models.ManyToManyField(Sport, related_name="fields")
+
+    def __str__(self):
+        return self.name
+
+
+class Training(models.Model):
+    field = models.ForeignKey(Field, related_name="field_trainings", on_delete=models.CASCADE)
+    sport = models.ForeignKey(Sport, related_name="sport_trainings", on_delete=models.CASCADE)
+    participants = models.ManyToManyField(User, related_name="participant_trainings", blank=True)
+    datetime = models.DateTimeField()
+
+    def __str__(self):
+        return f"Training at {self.field.name} on {self.datetime} for {self.sport}"
