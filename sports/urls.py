78de13aea321abd645dd5c39b37b0a5d089ba218
Denys Piuro <denys.pyuro@gmail.com>
2024-09-19 20:06:25 +0300

    Add Sport views and implement CRUD buttons

--- sports/urls.py
+++ sports/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from sports import views
+from sports.views import ToggleSubscriptionView
 
 urlpatterns = [
     path('', views.home, name='home'),  # Домашня сторінка
@@ -7,12 +8,15 @@ urlpatterns = [
     path('trainings/create/', views.TrainingCreateView.as_view(), name='training-create'),  # Створити тренування
     path('trainings/<int:pk>/update/', views.TrainingUpdateView.as_view(), name='training-update'),  # Оновити тренування
     path('trainings/<int:pk>/delete/', views.TrainingDeleteView.as_view(), name='training-delete'),  # Видалити тренування
-    path('trainings/<int:pk>/subscribe/', views.toggle_training_subscription, name='training-subscribe'),  # Підписка на тренування
+    path('<int:pk>/subscribe/', ToggleSubscriptionView.as_view(), name='training-subscribe'), # Підписка на тренування
 
     path('fields/', views.FieldListView.as_view(), name='field-list'),  # Список полів
     path('fields/create/', views.FieldCreateView.as_view(), name='field-create'),  # Створити поле
     path('fields/<int:pk>/update/', views.FieldUpdateView.as_view(), name='field-update'),  # Оновити поле
     path('fields/<int:pk>/delete/', views.FieldDeleteView.as_view(), name='field-delete'),  # Видалити поле
 
-    path('sport/', views.SportListView.as_view(), name='sport-list'),  # Список видів спорту
+    path('sport/', views.SportListView.as_view(), name='sport-list'),
+    path('sport/create/', views.SportCreateView.as_view(), name='sport-create'),
+    path('sport/<int:pk>/update/', views.SportUpdateView.as_view(), name='sport-update'),
+    path('sport/<int:pk>/delete/', views.SportDeleteView.as_view(), name='sport-delete'),
 ]
--- sports/views.py
+++ sports/views.py
@@ -1,21 +1,41 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views import generic
+from django.views.generic import View
+
 from .models import Training, Sport, Field
 
+
+User = get_user_model()
 
 class TrainingListView(generic.ListView):
     model = Training
     template_name = "training/training_list.html"
     context_object_name = "trainings"
 
+
+@method_decorator(login_required, name='dispatch')
 class TrainingCreateView(generic.CreateView):
     model = Training
-    fields = ['field', 'organizer', 'date', 'time']
+    fields = ['field', 'sport', 'datetime']
     template_name = "training/training_form.html"
     success_url = "/"
+
+    def form_valid(self, form):
+        # Зберігаємо форму, але не коммітимо
+        self.object = form.save(commit=False)
+        self.object.save()
+
+        # Додаємо користувача, який створив подію, до учасників
+        self.object.participants.add(self.request.user)
+
+        return super().form_valid(form)
+
 
 class TrainingUpdateView(generic.UpdateView):
     model = Training
@@ -29,6 +49,22 @@ class TrainingDeleteView(generic.DeleteView):
     success_url = "/"
 
 
+class ToggleSubscriptionView(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        # Отримуємо тренування за допомогою його primary key (pk)
+        training = get_object_or_404(Training, pk=pk)
+
+        # Отримуємо користувача з request
+        user = request.user
+
+        # Перевіряємо, чи користувач вже підписаний
+        if user in training.participants.all():
+            training.participants.remove(user)  # Видаляємо користувача
+        else:
+            training.participants.add(user)  # Додаємо користувача
+
+        # Перенаправляємо на список тренувань після виконання дії
+        return redirect('training-list')
 
 class FieldListView(generic.ListView):
     model = Field
@@ -60,27 +96,26 @@ class SportListView(generic.ListView):
     model = Sport
     template_name = "sport/sport_list.html"
     context_object_name = "sports"
-    queryset = Sport.objects.all()
+
+# Створення виду спорту
+class SportCreateView(generic.CreateView):
+    model = Sport
+    fields = ['name']
+    template_name = "sport/sport_form.html"
+    success_url = reverse_lazy('sport-list')
+
+# Оновлення виду спорту
+class SportUpdateView(generic.UpdateView):
+    model = Sport
+    fields = ['name']
+    template_name = "sport/sport_form.html"
+    success_url = reverse_lazy('sport-list')
+
+# Видалення виду спорту
+class SportDeleteView(generic.DeleteView):
+    model = Sport
+    template_name = "sport/sport_confirm_delete.html"
+    success_url = reverse_lazy('sport-list')
 
 def home(request):
     return render(request, 'home.html')
-
-
-from django.shortcuts import get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
-from .models import Training
-
-@login_required
-def toggle_training_subscription(request, pk):
-    training = get_object_or_404(Training, pk=pk)
-
-    # Отримуємо об'єкт User безпосередньо
-    user = User.objects.get(pk=request.user.pk)
-
-    # Перевіряємо, чи користувач вже є учасником тренування
-    if user in training.participants.all():
-        training.participants.remove(user)  # Відписати користувача
-    else:
-        training.participants.add(user)  # Підписати користувача
-
-    return redirect('training-list')
