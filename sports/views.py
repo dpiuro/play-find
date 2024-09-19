ae8175c289de48d09c4b4200c8ba2bdcbd3571da
Denys Piuro <denys.pyuro@gmail.com>
2024-09-19 12:16:29 +0300

    Add views for Training model

--- sports/views.py
+++ sports/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from audioop import reverse
 
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from sports.models import Training
+
+
+class TrainingListView(ListView):
+    model = Training
+    template_name = "training-list.html"
+    context_object_name = "training-list"
+    paginate_by = 5
+
+
+class TrainingCreateView(CreateView):
+    model = Training
+    fields = ["field", "sport", "date", "time", "participants"],
+    template_name = "training-form.html"
+    success_url = reverse_lazy("training_list")
+
+
+class TrainingUpdateView(UpdateView):
+    model = Training
+    fields = ["field", "sport", "date", "time", "participants"]
+    template_name = "training-form.html"
+    success_url = reverse_lazy("training-list")
+
+
+class TrainingDeleteView(DeleteView):
+    model = Training
+    template_name = "training_confirm_delete.html"
+    success_url = reverse_lazy("training-list")
+
+
+def subscribe_to_training(request, pk) -> HttpResponse:
+    training = get_object_or_404(Training, pk=pk)
+    training.participants.add(request.user)
+    return HttpResponseRedirect(reverse_lazy("training-list"))
