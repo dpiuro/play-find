879586b1d1c2fa85365bd36247199649c24b0a7a
Denys Piuro <denys.pyuro@gmail.com>
2024-09-19 15:16:36 +0300

    Add separate sidebar, implement login and create superuser

--- sports/urls.py
+++ sports/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('trainings/', views.TrainingListView.as_view(), name='training-list'),
     path('trainings/create/', views.TrainingCreateView.as_view(), name='training-create'),
+    path('trainings/<int:pk>/update/', views.TrainingUpdateView.as_view(), name='training-update'),
+    path('trainings/<int:pk>/delete/', views.TrainingDeleteView.as_view(), name='training-delete'),
+    path('trainings/<int:pk>/subscribe/', views.toggle_training_subscription, name='training-subscribe'),
     path('fields/', views.FieldListView.as_view(), name='field-list'),
     path('sport/', views.SportListView.as_view(), name='sport-list'),
 ]
--- sports/views.py
+++ sports/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from .models import Training, Sport, Field
 
@@ -13,6 +14,18 @@ class TrainingCreateView(generic.CreateView):
     fields = ['field', 'organizer', 'date', 'time']
     template_name = "training/training_form.html"
     success_url = "/"
+
+class TrainingUpdateView(generic.UpdateView):
+    model = Training
+    fields = ['field', 'organizer', 'date', 'time']
+    template_name = "training/training_form.html"
+    success_url = "/"
+
+class TrainingDeleteView(generic.DeleteView):
+    model = Training
+    template_name = "training/training_confirm_delete.html"
+    success_url = "/"
+
 
 
 class FieldListView(generic.ListView):
@@ -31,3 +44,11 @@ def home(request):
     return render(request, 'home.html')
 
 
+@login_required
+def toggle_training_subscription(request, pk):
+    training = get_object_or_404(Training, pk=pk)
+    if request.user in training.participants.all():
+        training.participants.remove(request.user)
+    else:
+        training.participants.add(request.user)
+    return redirect('training-list')
