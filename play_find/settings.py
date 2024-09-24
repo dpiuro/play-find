0a0fd2cb17b4306299fb91612df1c42259a2c45a
Denys Piuro <denys.pyuro@gmail.com>
2024-09-24 17:48:29 +0300

    Update settings

--- play_find/settings.py
+++ play_find/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-e5vpdaf^z_ozo3f!(j3504-8&$#fauq_!rik5gel0c2t*e2$or')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-e5vpdaf^z_ozo3f!(j3504-8&$#fauq_!rik5gel0c2t*e2$or')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
