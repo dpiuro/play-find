ff8b6e5df420bffbc81a2926ae21f0cc95b06354
Denys Piuro <denys.pyuro@gmail.com>
2024-09-24 18:35:49 +0300

    Add site adress to Allowed_hosts

--- play_find/settings.py
+++ play_find/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["127.0.0.1", 'localhost']
+ALLOWED_HOSTS = ["127.0.0.1", 'localhost', 'play-find.onrender.com']
 
 
 # Application definition
