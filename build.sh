6089561c7efc37ff9e57a7814b2660c2d5f41edb
Denys Piuro <denys.pyuro@gmail.com>
2024-09-24 21:19:04 +0300

    test commit

--- build.sh
+++ build.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
-# Додаємо Python в PATH
-export PATH="/opt/render/project/python/bin:$PATH"
-
 # Exit on error
 set -o errexit
 
 # Установити залежності
 pip install -r requirements.txt
 
-# Встановити Gunicorn
-pip install gunicorn
-
-# Зібрати статичні файли
+# Збираємо статичні файли
 python manage.py collectstatic --no-input
 
-# Виконати міграції
+# Виконуємо міграції
 python manage.py migrate
