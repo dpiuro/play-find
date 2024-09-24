db9ec8615b32700f035bb804cbcb96ef2d8cbbbb
Denys Piuro <denys.pyuro@gmail.com>
2024-09-24 13:28:41 +0300

    Update settings

--- play_find/settings.py
+++ play_find/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e5vpdaf^z_ozo3f!(j3504-8&$#fauq_!rik5gel0c2t*e2$or'
+SECRET_KEY = SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-e5vpdaf^z_ozo3f!(j3504-8&$#fauq_!rik5gel0c2t*e2$or')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
